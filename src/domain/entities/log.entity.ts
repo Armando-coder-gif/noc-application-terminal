54256c1a7ba1c1d0413f8cb2f74e4d9271aac5ff
Armando-coder-gif <armando.gonzalez@correo.unimet.edu.ve>
2026-01-12 09:04:54 -0600

    LogEntity

---
+++ src/domain/entities/log.entity.ts
@@ -0,0 +1,21 @@
+export enum LogSeverityLevel{
+    LOW = 'low',
+    MEDIUM = 'medium',
+    HIGH = 'high'
+}
+
+
+export class LogEntity{
+
+    public level: LogSeverityLevel; // Enum
+    public message: string;
+    public createdAt: Date;
+
+    constructor(message: string, level: LogSeverityLevel){
+        this.message = message;
+        this.level = level;
+        this.createdAt = new Date();
+
+    }
+
+}
